66bc32a3fc4a9fdfbcfe24281cb36d6858a85e26
slimeswamps <slimeswamp.the.millionth@gmail.com>
2026-01-28 20:31:49 +0000

    added feedback

--- MathGame.slimeswamps/Maths Game - C# Academy/Program.cs
+++ MathGame.slimeswamps/Maths Game - C# Academy/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MathsGame
 {
@@ -6,18 +7,16 @@ namespace MathsGame
     {
         static void Main(string[] args)
         {
-            // Welcomes the user
             Console.WriteLine("Welcome to the Maths Game!");
             Console.WriteLine("Press any key to start...");
             Console.ReadKey();
 
-            List<int> results = new List<int>();
+            List<string[]> results = new List<string[]>();
             bool playing = true;
             do
             {
-                // Gets the chosen operator from the user
                 Console.WriteLine("\nWhat game mode would you like to play?\nA - Addition\nS - Subtraction\nM - Multiplication\nD - Division\nR - View previous results\nQ - Exit game");
-                string mode = Console.ReadLine().ToUpper();
+                string? mode = Console.ReadLine().ToUpper();
                 while (mode != "A" && mode != "S" && mode != "D" && mode != "M" && mode != "R" && mode != "Q")
                 {
                     Console.WriteLine("\nInvalid operator.\nPlease choose from these:\nA - Addition\nS - Subtraction\nM - Multiplication\nD - Division\nR - View previous results\nQ - Exit game");
@@ -33,35 +32,34 @@ namespace MathsGame
                        int num1 = new Random().Next(1, 11);
                        int num2 = new Random().Next(1, 11);
                        int answer = 0;
-                       if (mode == "A")
+                       switch (mode)
                        {
-                           Console.WriteLine($"\nQuestion {i + 1}: What is {num1} + {num2}?");
-                           answer = num1 + num2;
-                       }
-                       else if (mode == "S")
-                       {
-                           Console.WriteLine($"\nQuestion {i + 1}: What is {num1} - {num2}?");
-                           answer = num1 - num2;
-                       }
-                       else if (mode == "M")
-                       {
-                           Console.WriteLine($"\nQuestion {i + 1}: What is {num1} * {num2}?");
-                            answer = num1 * num2;
-                       }
-                       else if (mode == "D")
-                       {
-                           //Ensures that the division is possible with integers
-                           float floatDivCheck = (float)num1/num2;
-                           float intDivCheck = num1/num2;
-                           while (floatDivCheck != intDivCheck)
-                           {
-                               num1 = new Random().Next(0, 101);
-                               num2 = new Random().Next(1, 11);
-                               floatDivCheck = (float)num1 / num2;
-                               intDivCheck = num1 / num2;
-                           }
-                           Console.WriteLine($"\nQuestion {i + 1}: What is {num1} / {num2}?");
-                           answer = num1 / num2;
+                            case "A":
+                                Console.WriteLine($"\nQuestion {i + 1}: What is {num1} + {num2}?");
+                                answer = num1 + num2;
+                                break;
+                            case "S":
+                                Console.WriteLine($"\nQuestion {i + 1}: What is {num1} - {num2}?");
+                                answer = num1 - num2;
+                                break;
+                            case "M":
+                                Console.WriteLine($"\nQuestion {i + 1}: What is {num1} * {num2}?");
+                                answer = num1 * num2;
+                                break;
+                            case "D":
+                                float floatDivCheck = (float)num1 / num2;
+                                float intDivCheck = num1 / num2;
+                                while (floatDivCheck != intDivCheck)
+                                {
+                                    num1 = new Random().Next(0, 101);
+                                    num2 = new Random().Next(1, 11);
+                                    floatDivCheck = (float)num1 / num2;
+                                    intDivCheck = num1 / num2;
+                                }
+                                Console.WriteLine($"\nQuestion {i + 1}: What is {num1} / {num2}?");
+                                answer = num1 / num2;
+                                break;
+
                        }
                        int userAnswer;
                        while (!int.TryParse(Console.ReadLine(), out userAnswer))
@@ -79,16 +77,35 @@ namespace MathsGame
                        }
                    }
                    Console.WriteLine($"Well done your score is: {score}");
-                   results.Add(score);
+                   results.Add(new string[2] {score.ToString(),mode});
                 }
                 else if (mode == "R")
                 {
-                   Console.WriteLine("\nYour previous results are:");
-                   for (int i = 0; i < results.Count(); i++)
-                   { 
-                        Console.WriteLine(results[i]);
-                   }
-                     
+                    DateTime date = DateTime.Now;
+                    Console.WriteLine($"\n{date}");
+                    Console.WriteLine("Your previous results are:");
+                    for (int i = 0; i < results.Count(); i++)
+                    {
+                        string[] tempResult = results[i];
+                        string resultMode = tempResult[1];
+                        switch (tempResult[1])
+                        {
+                            case "A":
+                                resultMode = "Addition";
+                                break;
+                            case "S":
+                                resultMode = "Subtraction";
+                                break;
+                            case "M":
+                                resultMode = "Multiplication";
+                                break;
+                            case "D":
+                                resultMode = "Division";
+                                break;
+                        }
+                        Console.WriteLine($"\t{resultMode} - {tempResult[0]}");
+                    }
+                    Console.ReadKey();
                 }
                 else if (mode == "Q")
                 {
